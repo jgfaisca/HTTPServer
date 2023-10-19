b5fb4947d243175e25aa34fbfe5ddda8a5c2d513
jgfaisca <jose.faisca@gmail.com>
2023-10-20 00:40:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,25 +8,17 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='SimpleHTTPSAuthServer',
-    version='1.1.0',
-    description='HTTPS server with Basic authentication and client certificate authentication.',
+    name='HTTPServer',
+    version='0.1',
+    description='HTTPS server.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/oza6ut0ne/SimpleHTTPSAuthServer',
+    url='https://github.com/jgfaisca/HTTPServer',
     license='MIT',
-    author='Ryota Okimatsu',
-    author_email='oza6ut0ne@gmail.com',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    py_modules=['SimpleHTTPSAuthServer']
+        ],
+    py_modules=['HTTPServer']
 )
