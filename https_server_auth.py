d7a62f7db368d79421d8996a3d5cc56cb5ff4fac
jgfaisca <jose.faisca@gmail.com>
2023-10-20 03:47:15 +0100

    Update https_server_auth.py

--- https_server_auth.py
+++ https_server_auth.py
@@ -99,7 +99,7 @@ class HTTPSAuthServer(Server):
                 self.certreqs = ssl.CERT_REQUIRED
 
             #self.socket = ssl.wrap_socket(self.socket, certfile=servercert,
-            self.socket = SSLContext.wrap_socket(self.socket, certfile=servercert,    
+            self.socket = ssl.SSLContext.wrap_socket(self.socket, certfile=servercert,    
                                           server_side=True,
                                           cert_reqs=self.certreqs,
                                           ca_certs=self.cacert)
