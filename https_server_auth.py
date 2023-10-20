e783b5d7b8bc9bf9bc87886ec54b19f347f1153e
jgfaisca <jose.faisca@gmail.com>
2023-10-20 03:42:56 +0100

    Update https_server_auth.py

--- https_server_auth.py
+++ https_server_auth.py
@@ -98,7 +98,8 @@ class HTTPSAuthServer(Server):
             if cacert is not None:
                 self.certreqs = ssl.CERT_REQUIRED
 
-            self.socket = ssl.wrap_socket(self.socket, certfile=servercert,
+            #self.socket = ssl.wrap_socket(self.socket, certfile=servercert,
+            self.socket = SSLContext.wrap_socket(self.socket, certfile=servercert,    
                                           server_side=True,
                                           cert_reqs=self.certreqs,
                                           ca_certs=self.cacert)
